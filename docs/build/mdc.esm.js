ed70c299853cfd127e3ec3035d1d15023edebaae
Quintin <qorsmond@gmail.com>
2019-06-07 20:48:06 +0200

    Publish build for docs pages

--- docs/build/mdc.esm.js
+++ docs/build/mdc.esm.js
@@ -1,6 +1,1 @@
-import { p as patchBrowser, g as globals, b as bootstrapLazy } from './mdc-f50c9b3a.js';
-
-patchBrowser().then(resourcesUrl => {
-  globals();
-  return bootstrapLazy([["demo-view",[[0,"demo-view"]]],["mdc-button",[[4,"mdc-button",{"raised":[4],"unelevated":[4],"outlined":[4],"dense":[4],"disabled":[4],"trailingIcon":[4,"trailing-icon"],"icon":[1],"label":[1]}]]],["mdc-icon",[[4,"mdc-icon"]]],["my-component",[[1,"my-component",{"first":[1],"middle":[1],"last":[1]}]]]], { resourcesUrl });
-});
+import{p as e,b as i}from"./p-a9cf0e7d.js";e().then(e=>i([["p-htlshtud",[[0,"demo-view"]]],["p-iivev422",[[4,"mdc-button",{raised:[4],unelevated:[4],outlined:[4],dense:[4],disabled:[4],trailingIcon:[4,"trailing-icon"],icon:[1],label:[1]}]]],["p-iuzqkveb",[[4,"mdc-icon"]]],["p-ztliaqls",[[1,"my-component",{first:[1],middle:[1],last:[1]}]]]],{resourcesUrl:e}));
---
+++ docs/build/p-iuzqkveb.entry.js
@@ -0,0 +1,1 @@
+import{r,h as s}from"./p-a9cf0e7d.js";class t{constructor(s){r(this,s)}render(){return s("i",{class:"material-icons"},s("slot",null))}static get style(){return""}}export{t as mdc_icon};
